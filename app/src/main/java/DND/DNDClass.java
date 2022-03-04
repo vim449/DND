18fdd154c6ead80be3f6391e46911465b725e37e
Corbanator <Corbandioxide42@gmail.com>
2022-03-04 10:49:27 -0700

    Add a DNDClass class to store a D&D class

---
+++ app/src/main/java/DND/DNDClass.java
@@ -0,0 +1,42 @@
+package DND;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class DNDClass {
+
+    // fields to store class-specific information
+
+    private String name;
+    private Feature[] features;
+    private int hitDie;
+    private Hashtable<Stat, Boolean> saves = new Hashtable<Stat, Boolean>();
+
+    private int skillProficiencyNum; // how many skill proficiencies
+    private String[] skillProficiencyOptions; // What options do they have for skill proficiencies?
+
+    private boolean spells; // Are they a spellcaster
+    private boolean prepareSpells; // Do they have to prepare spells
+    private boolean knowSpells; // Do they have a particular list of spells known
+
+    private int[][] spellSlotTable; // 2d array containing spell slots per level of spell per level of character.
+    private int[][] spellsKnownTable; // 2d array containing spells known per level of spell per level of character
+    private Stat mainStat;
+
+    public DNDClass(String name, Feature[] features, int hitDie, Hashtable<Stat, Boolean> saves,
+            int skillProficiencyNum, String[] skillProficiencyOptions, boolean spells, boolean prepareSpells,
+            boolean knowSpells, Stat mainStat) {
+
+        this.name = name;
+        this.features = features;
+        this.hitDie = hitDie;
+        this.saves = saves;
+        this.skillProficiencyNum = skillProficiencyNum;
+        this.skillProficiencyOptions = skillProficiencyOptions;
+        this.spells = spells;
+        this.prepareSpells = prepareSpells;
+        this.knowSpells = knowSpells;
+        this.mainStat = mainStat;
+    }
+
+}
