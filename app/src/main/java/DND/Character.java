4bc6c611b3cc65f8525cd9164741615e4b5a8a19
Thomas Dick <thomas.ed.dick@gmail.com>
2022-03-04 14:26:49 -0700

    method getClasses is working now

--- app/src/main/java/DND/Character.java
+++ app/src/main/java/DND/Character.java
@@ -4,110 +4,61 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 public class Character {
-  public enum Alignment {
-    LAWFUL_GOOD,
-    LAWFUL_NEUTRAL,
-    LAWFUL_EVIL,
-    CHAOTIC_GOOD,
-    CHAOTIC_NETRUAL,
-    CHAOTIC_EVIL,
-    NEUTRAL,
-    NEUTRAL_GOOD,
-    NEUTRAL_EVIL
-  }
-
-  private DNDClass dndclass;
-  private String subclass;
-  private String race;       // TODO, make a custom class for this
-  private String background; // TODO, make a custom class for this
-  private char[] ideals;
-  private char[] bonds;
-  private char[] traits;
-  private char[] flaws;
-  private char[] name;
-  private short age;
-  private short height; // inches
-  private Alignment alignment;
-  private int experience;
-  private byte proficiencyBonus;
-
-  private Hashtable<Stat, Byte> stats = new Hashtable<>();
-  private Hashtable<Stat, Byte> modifiers = new Hashtable<>();
-  private byte AC;
-  private byte initiative;
-  private boolean inspiration; // rn, only level 1 characters will be supported,
-  // so this will always be null
-  private String hitdice; // dice class?
-  private short walkSpeed;
-  private short flySpeed;
-  private short swimSpeed;
-
-  private byte[] spellSlots = new byte[9];
-  private short spellsKnown;
-  private byte prepareCount;
-  private byte spellDC;
-  private byte spellAttack;
-  private Vector<String> cantrips = new Vector<String>(0);
-  private Vector<Vector<String>> spells = new Vector<Vector<String>>(0);
-
-  private Hashtable<Ability, Byte> abilities = new Hashtable<>();
-  private Hashtable<Ability, Profs> abilityProficiences = new Hashtable<>();
-  // I'm not sold on storing passive stats as fields
-  private Hashtable<Ability, Byte> passives = new Hashtable<>();
-  private Hashtable<Stat, Profs> saveProficiencies = new Hashtable<>();
-  private Hashtable<Stat, Byte> saves = new Hashtable<>;
-  private String[] proficiencies;
-  private Equipment[] equipment;
-  private Feature[] features;
-  private short maxhp;
-  private int gp;
-
-  public Character(DNDClass dndclass, String race, String background,
-                   char[] name, Alignment alignment) {
-    this.dndclass = dndclass;
-    this.race = race;
-    this.background = background;
-    this.name = name;
-    this.alignment = alignment;
-
-    experience = 0;
-    proficiencyBonus = 2;
-    swimSpeed = 0;
-    flySpeed = 0;
-    inspiration = false;
-
-    spellsKnown = 0;
-    spellSlots = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    prepareCount = 0;
-    spellDC = 0;
-    spellAttack = 0;
-
-    traits = "";
-    bonds = "";
-    flaws = "";
-    ideals = "";
-    age = 0;
-    height = 0;
-
-    calcModifiers();
-    AC = 10 + modifiers.get(Stats.DEXTERITY);
-    initiative = modifiers.get(Stats.DEXTERITY);
-
-    hitdice = this.dndclass.getHitDice();
-    saveProficiencies = this.dndclass.getSaveProfs();
-    saves = calcSaves();
-  }
-
-  private calcModifiers() {
-    for (Stat key : this.stats.keySet()) {
-      modifiers.put(key, (stats.get(key) - 10) / 2);
+    public enum Alignment {
+        LAWFUL_GOOD,
+        LAWFUL_NEUTRAL,
+        LAWFUL_EVIL,
+        CHAOTIC_GOOD,
+        CHAOTIC_NETRUAL,
+        CHAOTIC_EVIL,
+        NEUTRAL,
+        NEUTRAL_GOOD,
+        NEUTRAL_EVIL
     }
-  }
 
-  private calcSaves() {
-    for (Stat stat : Stat.values()) {
-      saves.put(stat, proficiencyBonus * saveProficiencies.get(stat) +
-                          modifiers.get(stat));
-    }
-  }
+    private String dndclass;
+    private String subclass;
+    private String race; // TODO, make a custom class for this
+    private String background; // TODO, make a custom class for this
+    private String ideals;
+    private String bonds;
+    private String traits;
+    private String flaws;
+    private String name;
+    private short age;
+    private short height; // inches
+    private Alignment alignment;
+    private int experience;
+    private byte proficiency_bonus;
+
+    private Hashtable<Stat, Byte> stats = new Hashtable<>();
+    private Hashtable<Stat, Byte> modifiers = new Hashtable<>();
+    private byte AC;
+    private byte initiative;
+    private boolean inspiration; // rn, only level 1 characters will be supported,
+    // so this will always be null
+    private String hitdice; // dice class?
+    private short walkSpeed;
+    private short flySpeed;
+    private short swimSpeed;
+
+    private byte[] spellSlots = new byte[9];
+    private short spellsKnown;
+    private byte prepareCount;
+    private byte spellDC;
+    private byte spellAttack;
+    private Vector<String> cantrips = new Vector<String>(0);
+    private Vector<Vector<String>> spells = new Vector<Vector<String>>(0);
+
+    private Hashtable<String, Byte> abilities = new Hashtable<>();
+    private Hashtable<String, Profs> ability_proficiences = new Hashtable<>(); // I'm not sold on storing passive stats
+                                                                               // as
+    // fields
+    private Hashtable<String, Byte> passives = new Hashtable<>();
+    private Hashtable<Stat, Profs> saving_throws = new Hashtable<>();
+    private String[] proficiencies;
+    private Equipment[] equipment;
+    private Feature[] features;
+    private short maxhp;
+    private int gp;
 }
--- app/src/main/java/DND/PdfParse.java
+++ app/src/main/java/DND/PdfParse.java
@@ -1,27 +1,58 @@
 package DND;
 
 import java.io.IOException;
+import java.util.Hashtable;
 
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.parser.PdfTextExtractor;
 
+import java.util.regex.*;
+
 public class PdfParse {
-    String filepath = "";
+    private String filepath = "";
+    private Hashtable<String, Integer> classesTable = new Hashtable<>();
 
     public PdfParse(String inputFilePath) {
         this.filepath = inputFilePath;
     }
 
-    public String reuturnObject() {
+    public Hashtable<String, Integer> getClasses(int tableOfContentsPage) {
         String pdfText = "";
         try {
             PdfReader reader = new PdfReader(filepath);
             PdfTextExtractor text = new PdfTextExtractor(reader);
-            pdfText = text.getTextFromPage(188);
+            pdfText = text.getTextFromPage(tableOfContentsPage);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(pdfText);
-        return pdfText;
+
+        // stip the string off all whitespace and periods
+        pdfText = pdfText.replaceAll(" ", "");
+        pdfText = pdfText.replaceAll("\\.", "");
+
+        // find all the classes in the table of contents
+        Pattern p1 = Pattern.compile("Classes(.*?)Chapter", Pattern.DOTALL);
+        Matcher classes = p1.matcher(pdfText);
+
+        // check to see if there is a match
+        if (classes.find()) {
+            String classesString = classes.group(0);
+
+            // create the regex for parsing the classes string
+            Pattern p = Pattern.compile("([0-9]+)");
+            String strings[] = classesString.split("\\n");
+
+            // grab the last groups of numbers from the matched groups
+            for (int i = 0; i < strings.length; i++) {
+                Matcher matches = p.matcher(strings[i]);
+                if (matches.find()) {
+                    int pageNum = Integer.valueOf(matches.group(matches.groupCount()));
+
+                    classesTable.put(strings[i].replaceAll(matches.group(matches.groupCount()), ""), pageNum);
+                }
+            }
+        }
+        return classesTable;
     }
+
 }
