13e87074020ff623ad1163f7291be9436213c44f
Dominic Adamson <adamson.dom@gmail.com>
2022-03-04 10:07:52 -0700

    make single-use enums part of class

--- app/src/main/java/DND/Feature.java
+++ app/src/main/java/DND/Feature.java
@@ -1,22 +1,35 @@
 package DND;
 
 public class Feature {
-    private String name;
-    private String descriptionText;
-    private int level;
-    private int uses;
-    private Rest regainCondition;
-    private String regainConditionRoll; // only used if the regain condition has to occur a speciic number of times
+  public enum Rest {
+    LONG,
+    SHORT,
+    DAY,
+    ROLLED_LONG,
+    ROLLED_SHORT,
+    ROLLED_DAY,
+    NULL
 
-    public Feature(String name, String descriptionText, int level, int uses, Rest regainCondition) {
-        this.name = name;
-        this.descriptionText = descriptionText;
-        this.level = level;
-        this.uses = uses;
-        this.regainCondition = regainCondition;
-    }
+  }
 
-    public Feature(String name, String descriptionText) {
-        new Feature(name, descriptionText, 0, 0, Rest.NULL);
-    }
+  private String name;
+  private String descriptionText;
+  private int level;
+  private int uses;
+  private Rest regainCondition;
+  private String regainConditionRoll; // only used if the regain condition has
+                                      // to occur a speciic number of times
+
+  public Feature(String name, String descriptionText, int level, int uses,
+                 Rest regainCondition) {
+    this.name = name;
+    this.descriptionText = descriptionText;
+    this.level = level;
+    this.uses = uses;
+    this.regainCondition = regainCondition;
+  }
+
+  public Feature(String name, String descriptionText) {
+    new Feature(name, descriptionText, 0, 0, Rest.NULL);
+  }
 }
