7df664fb5c30ba5759a73ef1591f200a699b386e
Dominic Adamson <adamson.dom@gmail.com>
2022-03-04 10:38:12 -0700

    having rolls throw exceptions was too much of hassle

--- app/src/main/java/DND/Roll.java
+++ app/src/main/java/DND/Roll.java
@@ -3,33 +3,29 @@ package DND;
 import java.util.Random;
 
 public class Roll {
-    private Random random;
+  private Random random;
 
-    public Roll() {
-        random = new Random(System.currentTimeMillis());
+  public Roll() { random = new Random(System.currentTimeMillis()); }
+
+  public Roll(long seed) { random = new Random(seed); }
+
+  public int roll(String rollString) {
+    rollString = rollString.trim();
+    if (!rollString.contains("d")) {
+      System.out.println("dice rolls must be of the form XdY or XdY+Z");
     }
 
-    public Roll(long seed) {
-        random = new Random(seed);
+    String[] dice = rollString.split("d", -1);
+    dice[1] = dice[1].split("\\+", 0)[0];
+    int modifier = 0;
+    if (rollString.contains("+")) {
+      modifier = Integer.valueOf(rollString.split("\\+", -1)[1]);
+    }
+    int roll = 0;
+    for (int i = 0; i < Integer.valueOf(dice[0]); i++) {
+      roll += (random.nextInt(Integer.valueOf(dice[1])) + 1);
     }
 
-    public int roll(String rollString) throws FormatException {
-        rollString = rollString.trim();
-        if (!rollString.contains("d")) {
-            throw new FormatException("dice rolls must be of the form XdY or XdY+Z");
-        }
-
-        String[] dice = rollString.split("d", -1);
-        dice[1] = dice[1].split("\\+", 0)[0];
-        int modifier = 0;
-        if (rollString.contains("+")) {
-            modifier = Integer.valueOf(rollString.split("\\+", -1)[1]);
-        }
-        int roll = 0;
-        for (int i = 0; i < Integer.valueOf(dice[0]); i++) {
-            roll += (random.nextInt(Integer.valueOf(dice[1])) + 1);
-        }
-
-        return roll + modifier;
-    }
+    return roll + modifier;
+  }
 }
