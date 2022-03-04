c25897f81109610302a618535850363d560117cc
Thomas Dick <thomas.ed.dick@gmail.com>
2022-03-04 08:43:18 -0700

    pdf page parser working finally

--- app/src/main/java/DND/PdfParse.java
+++ app/src/main/java/DND/PdfParse.java
@@ -1,6 +1,9 @@
 package DND;
 
-import com.aspose.pdf.*;
+import java.io.IOException;
+
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.parser.PdfTextExtractor;
 
 public class PdfParse {
     String filepath = "";
@@ -9,30 +12,16 @@ public class PdfParse {
         this.filepath = inputFilePath;
     }
 
-    public void reuturnObject() {
-        // The path to the documents directory.
-        String _dataDir = "C:/Users/thoma/Documents/Java Projects/DND/app/src/main/java/DND/";
-        String filePath = _dataDir + "D&D 5e - Players Handbook.pdf";
-
-        // Open document
-        com.aspose.pdf.Document pdfDocument = new com.aspose.pdf.Document(filePath);
-
-        // Create TextAbsorber object to extract text
-        com.aspose.pdf.TextAbsorber textAbsorber = new com.aspose.pdf.TextAbsorber();
-
-        // Accept the absorber for all the pages
-        pdfDocument.getPages().accept(textAbsorber);
-
-        // Get the extracted text
-        String extractedText = textAbsorber.getText();
+    public String reuturnObject() {
+        String pdfText = "";
         try {
-            java.io.FileWriter writer = new java.io.FileWriter(_dataDir + "extracted-text.txt", true);
-            // Write a line of text to the file
-            writer.write(extractedText);
-            // Close the stream
-            writer.close();
-        } catch (java.io.IOException e) {
+            PdfReader reader = new PdfReader(filepath);
+            PdfTextExtractor text = new PdfTextExtractor(reader);
+            pdfText = text.getTextFromPage(188);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(pdfText);
+        return pdfText;
     }
 }
